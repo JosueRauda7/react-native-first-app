f7082818d2f8d05b43e477cdb25c23a5bc7fef4c
JosueRauda7 <katerozarack@gmail.com>
2020-11-25 06:47:58 -0600

    Primer app en react native <3

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [outputText, setOutputText] = useState(
+    "Bienvenido. No presiones el botón. D:"
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{outputText}</Text>
+      <Button
+        style={styles.button}
+        title='Change Text'
+        onPress={() => setOutputText("Lo presionastes :c")}
+      />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,8 +23,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    flex: 2,
+    marginTop: 10,
   },
 });
